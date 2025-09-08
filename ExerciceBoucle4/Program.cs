1a110276526b714a7a6803e4f6e343d8bbe946cf
yoann <yoanngervais.yg13750@gmail.com>
2025-09-08 14:30:17 +0200

    fini

---
+++ ExerciceBoucle4/Program.cs
@@ -0,0 +1,21 @@
+﻿Random aleatoire = new Random();
+int nbrMystere = aleatoire.Next(1, 100);
+int nbrUtilisateur = -1;
+
+do
+{
+    Console.Write("Devinez le nombre mystère : ");
+    nbrUtilisateur = Convert.ToInt32(Console.ReadLine()!);
+
+    if (nbrUtilisateur < nbrMystere)
+    {
+        Console.WriteLine("Le nombre mystère est plus grand");
+    }
+    else if (nbrUtilisateur > nbrMystere)
+    {
+        Console.WriteLine("Le nombre mystère est plus petit");
+    }
+
+} while (nbrUtilisateur != nbrMystere);
+
+Console.WriteLine("Bravo, vous avez trouvé le nombre mystère !");
