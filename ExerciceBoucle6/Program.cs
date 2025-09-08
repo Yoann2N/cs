0775208ef4033aef099ac81d271ba97e64d5509c
yoann <yoanngervais.yg13750@gmail.com>
2025-09-08 14:02:22 +0200

    fini

---
+++ ExerciceBoucle6/Program.cs
@@ -0,0 +1,9 @@
+﻿ int[] nombres = { 3, 7, 2, 9, 4 };
+        int somme = 0;
+
+        foreach (int n in nombres)
+        {
+            somme += n;
+        }
+
+        Console.WriteLine($"La somme des éléments du tableau est : {somme}");
