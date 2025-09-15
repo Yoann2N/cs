e650bff905112580b505a14410b64b2a44ad11d9
yoann <yoanngervais.yg13750@gmail.com>
2025-09-15 12:09:18 +0200

    Création class Compte

--- ExercicePOO/Compte.cs
+++ ExercicePOO/Compte.cs
@@ -1,4 +1,19 @@
 ﻿class Compte
 {
-    
+    public string Solde
+    {
+        get
+        {
+            return _solde;
+        }
+
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Le solde ne peut pas être négatif.");
+            }
+            _solde = value;
+        }
+    }
 }
