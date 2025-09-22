15dd0b026bc0b7291a26f5877df48f3c3846f893
yoann <yoanngervais.yg13750@gmail.com>
2025-09-22 09:01:54 +0200

    heritage

--- heritage/Program.cs
+++ heritage/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class heritage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
